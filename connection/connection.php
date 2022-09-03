4a67ac0ba7625342e9df6c9da36a9788aeafd6f0
Jawad <jawad.php@gmail.com>
2022-09-03 00:44:18 -0400

    Updated ReadMe

--- connection/connection.php
+++ connection/connection.php
@@ -13,8 +13,7 @@ if ($hostName == "localhost") {
 	$user = defined ('LOCAL_USER') ? LOCAL_USER : "---LOCAL DATABASE USERNAME---";
 	$password = defined ('LOCAL_PWD') ? LOCAL_PWD : "---LOCAL DATABASE PASSWORD---";
 }
-else {			
-	// TEST: https://quran-similarity-score.000webhostapp.com/api/verse.php?surah_number=10&aya_number=49
+else {				
 	$database = defined ('REMOTE_DB') ? REMOTE_DB : "---REMOTE DATABASE NAME---";
 	$host = defined ('REMOTE_HOST') ? REMOTE_HOST : "---REMOTE DATABASE HOST---";
 	$user = defined ('REMOTE_USER') ? REMOTE_USER : "---REMOTE DATABASE USERNAME---";
