b4b96fd0d617a6142b6e2b2345e2d20820135ddc
Jawad <jawad.php@gmail.com>
2025-02-20 16:09:02 -0500

    Fixed sec

--- api/google-translate.php
+++ api/google-translate.php
@@ -11,11 +11,11 @@ http://localhost/quran/api/google-translate.php?text=الله
 */
 
 include_once ("api.php");
+include_once ("../connection/pwd/pwd.php");
 
 // Turn cache on or off
 // This is useful for testing purposes
 $cacheOn = false;
-define ("GOOGLE_TRANSLATE_API_KEY", "AIzaSyClMeu7inyMNSHBtZoGd_kMpeW3YQOoy04");
 $text = isset ($_GET['text']) ? make_safe($_GET['text']) : NULL;
 
 // Cache adapter for phpFastCache
@@ -36,88 +36,88 @@ $cache = \Phpfastcache\CacheManager::getInstance('Files');
 // $cache->deleteItem($inst);
 
 if ($cacheOn) {
-	// Get instance of the cache
-	$cachedInstance = $cache->getItem('google-translate.php?text='.$text);
-	$isCached = !(is_null ($cachedInstance->get ()));	
+    // Get instance of the cache
+    $cachedInstance = $cache->getItem('google-translate.php?text='.$text);
+    $isCached = !(is_null ($cachedInstance->get ()));	
 } else {
-	$isCached = false;
+    $isCached = false;
 }
 
 // Fetch from database if not cached
 $translated = '';
 if (!$isCached) {
-	$error=false;
-	if (!$error) {
-		if (strlen ($text) === 0) {
-			$error = true;
-			$reason = "There is no text provided to translate.";
-		}
+    $error=false;
+    if (!$error) {
+        if (strlen ($text) === 0) {
+            $error = true;
+            $reason = "There is no text provided to translate.";
+        }
 
-		if (($_SERVER['HTTP_REFERER'] != "https://www.quran-ml.com/") && ($_SERVER['HTTP_REFERER'] != "http://localhost:3000/")) {
-			$error = true;
-			// This request is coming from an unknown domain. Give bogus error.
-			$reason = "SSL identity invalid";
-		}
+        if (($_SERVER['HTTP_REFERER'] != "https://www.quran-ml.com/") && ($_SERVER['HTTP_REFERER'] != "http://localhost:3000/")) {
+            $error = true;
+            // This request is coming from an unknown domain. Give bogus error.
+            $reason = "SSL identity invalid";
+        }
 
-		if (!$error) {			
-			$url = "https://translation.googleapis.com/language/translate/v2?key=".GOOGLE_TRANSLATE_API_KEY."&q=".urlencode($text)."&target=en";
-			
-			// create & initialize a curl session
-			$curl = curl_init();
+        if (!$error) {			
+            $url = "https://translation.googleapis.com/language/translate/v2?key=".GOOGLE_TRANSLATE_API_KEY."&q=".urlencode($text)."&target=en";
+            
+            // create & initialize a curl session
+            $curl = curl_init();
 
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($curl, CURLOPT_HEADER, false);
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curl, CURLOPT_HEADER, false);
 
-			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-			
-			// $output contains the output string
-			$response = curl_exec($curl);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+            
+            // $output contains the output string
+            $response = curl_exec($curl);
 
-			// print_r ($response);		
-			$response = json_decode ($response, true);
+            // print_r ($response);		
+            $response = json_decode ($response, true);
 
-			$error = curl_error($curl);
-			// close curl resource to free up system resources
-			// (deletes the variable made by curl_init)						
-			curl_close($curl);
-				
-			if ($error) {
-			  $reason = "cURL Error #:" . $error;
-			} else {	
-			  $translated = $response['data']['translations'][0]['translatedText'];
-			}			
-		}
+            $error = curl_error($curl);
+            // close curl resource to free up system resources
+            // (deletes the variable made by curl_init)						
+            curl_close($curl);
+                
+            if ($error) {
+              $reason = "cURL Error #:" . $error;
+            } else {	
+              $translated = $response['data']['translations'][0]['translatedText'];
+            }			
+        }
 
-	}
+    }
 
-	if ($error) {
-		$json = '{ "error": "'.$reason.'" }';
-	} else {
-				
-		
-		$json = '{';
-		$json .= 		
+    if ($error) {
+        $json = '{ "error": "'.$reason.'" }';
+    } else {
+                
+        
+        $json = '{';
+        $json .= 		
 '"text": "'.$text.'",
 "translated": "'.$translated.'"';
-		$json .= '}';		
+        $json .= '}';		
 
-		// $json = json_encode ($json);
+        // $json = json_encode ($json);
 
-	    // Save the json data to the cache 
-		if ($cacheOn) {
-			$cache->save(
-				// Expire in 5 days
-		        $cachedInstance->set($json)->expiresAfter(432000)
-		    );	
-		}
-	    
-	}	
+        // Save the json data to the cache 
+        if ($cacheOn) {
+            $cache->save(
+                // Expire in 5 days
+                $cachedInstance->set($json)->expiresAfter(432000)
+            );	
+        }
+        
+    }	
 } 
 // Fetch from cache
 else {
-	$json = $cachedInstance->get();
+    $json = $cachedInstance->get();
 }
 
 echo $json;
